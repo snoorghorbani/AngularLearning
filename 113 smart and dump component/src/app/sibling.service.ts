1159d55d69e1272a83dbe1d700e2a79a55679f1f
pegah <pegah.khf@gmail.com>
2017-07-10 09:41:09 +0430

    113 smart and dump component

---
+++ 113 smart and dump component/src/app/sibling.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class siblingService {
+    state: number = 1;
+    increment(): number {
+        return ++this.state;
+    }
+    decrement(): number {
+        return --this.state;
+    }
+}
---
+++ 113 smart and dump component/src/app/smartComponent/sibling/sibling.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { siblingService } from '../../sibling.service';
+
+@Component({
+  selector: 'app-sibling',
+  templateUrl: './sibling.component.html',
+  styleUrls: ['./sibling.component.css']
+
+})
+export class SiblingComponent {
+  constructor(private SiblingService: siblingService) {
+    this.SiblingService.increment();
+  }
+}
